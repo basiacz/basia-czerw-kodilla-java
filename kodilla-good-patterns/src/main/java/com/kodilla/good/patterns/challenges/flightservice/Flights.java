449d2201b1f5db5a2b1ff14ff5361c0c9cf9cbac
“basiacz <basiaczerw@gmail.com>
2017-12-23 18:33:02 +0100

    9.5&10

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flightservice/Flights.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flightservice/Flights.java
@@ -7,7 +7,7 @@ public class Flights {
     public static List<Flight> getFlights(){
         List<Flight> flights = new ArrayList<>();
         flights.add(new Flight("KRK", "KTW"));
-        flights.add(new Flight("TTT", "KKK"));
-        return flights;
+        flights.add(new Flight("WRO", "KRK"));
+            return flights;
     }
 }
