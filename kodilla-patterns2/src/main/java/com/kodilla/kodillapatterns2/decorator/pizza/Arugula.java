2441d463c2bf46efe6ff3566d0bbf1eaea682275
basiacz <basiaczerw@gmail.com>
2018-04-29 15:46:37 +0200

    20.2

---
+++ kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/decorator/pizza/Arugula.java
@@ -0,0 +1,20 @@
+package com.kodilla.kodillapatterns2.decorator.pizza;
+
+import java.math.BigDecimal;
+
+public class Arugula extends AbstractPizzaOrderDecorator {
+
+    public Arugula(PizzaOrder pizzaOrder) {
+        super(pizzaOrder);
+    }
+
+    @Override
+    public BigDecimal getCost() {
+        return super.getCost().add(new BigDecimal(5));
+    }
+
+    @Override
+    public String getIngredients() {
+        return super.getIngredients() + ", arugula";
+    }
+}
